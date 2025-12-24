3cf29a4a99587fb652d614a85fd0e9bcce4f3273
aashisrjl <aashis252@gmail.com>
2025-12-24 10:40:18 +0545

    update(userModel): add column in user table

--- backend/models/user/userModel.js
+++ backend/models/user/userModel.js
@@ -4,39 +4,40 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: { isEmail: true }
     },
-
     password: {
       type: DataTypes.STRING,
       allowNull: true // Google login
     },
-
     googleId: {
       type: DataTypes.STRING,
       allowNull: true
     },
-
     profileImage: {
       type: DataTypes.STRING,
       allowNull: true
     },
-
     role: {
       type: DataTypes.ENUM("user"),
       defaultValue: "user"
     },
-
+    device:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    location:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
     isActive: {
       type: DataTypes.BOOLEAN,
       defaultValue: true
     },
-
     isVerified: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
