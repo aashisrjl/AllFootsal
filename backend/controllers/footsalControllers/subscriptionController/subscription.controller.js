b4d31a42310d84514f865d150969bd2fa55d21f5
aashisrjl <aashis252@gmail.com>
2026-03-04 21:11:20 +0545

    update: Subscrip;tion add controller

--- backend/controllers/footsalControllers/subscriptionController/subscription.controller.js
+++ backend/controllers/footsalControllers/subscriptionController/subscription.controller.js
@@ -22,17 +22,61 @@ const getFutsalSubscription = async (req,res)=>{
 }
 
 const addFutsalSubscription = async (req,res)=>{
-  
+  const {subscription_plan} = req.params
     const futsalId = req.futsalId
-    const existed = await Subscription.findAll({
+    const existed = await Subscription.findOne({
         where:{
             futsal_id: futsalId
         }
     })
+    const Datenow = new Date();
+    const DateEnd = new Date(now);
+    const subscription_price=0.0;
+    if(subscription_plan == "trial"){
+        if (existed?.is_trial) {
+      return res.status(409).json({
+         message: "Trial already used." 
+        });
+    }
+        DateEnd.setDate(DateEnd.getDate() + 14); //trial
+        subscription_price = 0.0;
+    }else if(subscription_plan == "monthly"){
+         DateEnd.setDate(DateEnd.getDate() + 30); // monthly=15
+         subscription_price= 1200.00;
+    }else if(subscription_plan == "half-yearly"){
+        DateEnd.setDate(DateEnd.getDate() + 180); // half-yearly
+        subscription_price = 6000.00;
+    }else if(subscription_plan == "yearly"){
+        DateEnd.setDate(DateEnd.getDate() + 360); // yearaly
+        subscription_price= 10000.00;
+    }else{
+        res.status(400).json({
+            message: "select the correct plan"
+        })
+    }
 
+    const subs = await Subscription.Create({
+        subscription_plan,
+        subscription_start:Datenow,
+        subscription_end:DateEnd,
+        subscription_fee : subscription_price,
+        status: "active"
+    })
+
+    res.status(200).json({
+        message: "subscription created sucessfully"
+    })
+   
     if(existed){
-        return res.status(400).json({
-            message: "you already created a subscription"
+        Subscription.subscription_plan = subscription_plan;
+        Subscription.subscription_start= Datenow;
+        Subscription.subscription_end= DateEnd;
+        Subscription.subscription_fee= subscription_price;
+        Subscription.status = "active";
+        await Subscription.save();
+
+        res.status(400).json({
+            message: "Successfully updated a subscription"
         })
     }
 
