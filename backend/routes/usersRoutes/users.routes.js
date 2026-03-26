28f8bbf70ee0943e0e97e5334cc68b7c3016bcc5
aashisrjl <aashisrijal252@gmail.com>
2026-03-26 20:34:25 +0545

    update: user routes handle

--- backend/routes/usersRoutes/users.routes.js
+++ backend/routes/usersRoutes/users.routes.js
@@ -5,11 +5,39 @@ const isBothAuthenticated = require('../../middleware/authMiddleware/bothAuthent
 const isUserAuthenticated = require('../../middleware/authMiddleware/userAuthenticated');
 const { upload } = require('../../services/multer/multerConfig');
 
-router.get('/users', getAllUsers);
-router.get('/users/:id', isBothAuthenticated, getUserById);
-router.get('/users/profile', isUserAuthenticated, getProfile);
-router.put('/users/profile', isUserAuthenticated, updateProfile);
-router.put('/users/profile/image', isUserAuthenticated, upload.single('profileImage'), updateProfileImage);
-router.delete('/users/profile/image', isUserAuthenticated, deleteProfileImage);
+router.get(
+    '/users', 
+    getAllUsers
+);
+router.get(
+    '/users/:id', 
+    isBothAuthenticated,
+     getUserById
+    );
+
+router.get(
+    '/users/profile', 
+    isUserAuthenticated, 
+    getProfile
+);
+
+router.put(
+    '/users/profile', 
+    isUserAuthenticated, 
+    updateProfile
+);
+
+router.put(
+    '/users/profile/image', 
+    isUserAuthenticated, 
+    upload.single('profileImage'), 
+    updateProfileImage
+);
+
+router.delete(
+    '/users/profile/image', 
+    isUserAuthenticated, 
+    deleteProfileImage
+);
 
 module.exports = router;
